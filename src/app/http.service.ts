59b0ae0bae9589a3189ab1771daf56a6a7c7afa6
JosehGaks <josehwilddog@gmail.com>
2020-01-17 14:42:45 +0300

    create a get function

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { getLocaleDayNames } from '@angular/common';
 
 
 @Injectable({
@@ -9,5 +10,10 @@ export class HttpService {
 
   constructor(
     private http: HttpClient
-  ) { }
+  ) {}
+
+    getGithubNames(){
+      return this.http.get()
+    }
+   
 }
