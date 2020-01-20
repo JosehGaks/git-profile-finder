8cc6b034188692ce91ba4db7598b1dacffdf934a
JosehGaks <josehwilddog@gmail.com>
2020-01-20 10:28:36 +0300

    add current service to the providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HttpService } from './http.service';
 
 @NgModule({
   declarations: [
@@ -19,7 +21,7 @@ import { HomeComponent } from './home/home.component';
     FormsModule,
     HttpClientModule 
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
