24685400b3a14c904a3dab7b31024b01097c2f80
JosehGaks <josehwilddog@gmail.com>
2020-01-20 08:46:41 +0300

    create a profile service

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -8,12 +8,26 @@ import { getLocaleDayNames } from '@angular/common';
 })
 export class HttpService {
 
+  private username:string;
+  private clientid = 'efba229516318213923e'
+  private clientsecret = 'a92cca1f393f386d27353d723118c5a8e03325d6'
+
   constructor(
     private http: HttpClient
   ) {}
 
-    getGithubNames(){
-      return this.http.get('https://api.github.com/users/?access_token=+f9688f62bffff9402e4311a7e0a8807c30c8a837')
+    getProfileInfo(){
+      return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+      .map(res => res.json());
+    }
+  
+    getProfileRepos(){
+      return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+      .map(res => res.json());
+    }
+  
+    updateProfile(username:string){
+      this.username = username;
     }
    
 }
